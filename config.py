2ed5b84f07e8a0a0dc7ed4d97c8716f17d778c8d
ivnviv <vanya0774@gmail.com>
2025-05-04 21:55:33 +0300

    small fixes

--- config.py
+++ config.py
@@ -3,5 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = int(os.getenv("CHAT_ID"))
+# Чтение переменной AMVERA
+amvera_var = os.getenv("AMVERA")  # Лучше использовать get() на случай отсутствия переменной
+
+# Проверка, что значение переменной "AMVERA" равно "1"
+if amvera_var == "1":
+    print("Работаю в облаке Амвера")
+    TOKEN = os.environ["TOKEN"]
+    CHAT_ID = int(os.environ["CHAT_ID"])
+else:
+    print("Работаю локально")
+    TOKEN = os.getenv("TOKEN")
+    CHAT_ID = int(os.getenv("CHAT_ID", "0"))  # Если переменная отсутствует, можно задать дефолтное значение
